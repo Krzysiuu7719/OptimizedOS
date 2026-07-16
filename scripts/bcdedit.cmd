60a7b0852f74c6ad1ae4e14b9e1a8e520b7eea47
Krzysi_x <104031490+Krzysiuu7719@users.noreply.github.com>
2026-07-16 10:36:05 +0200

    Update bcdedit.cmd

--- scripts/bcdedit.cmd
+++ scripts/bcdedit.cmd
@@ -1,10 +1,10 @@
 @echo off
 title OptimizedOS - BCDEdit Tweaks
 
-bcdedit /set useplatformtick yes
-bcdedit /set useplatformlock no
-bcdedit /set tscsyncpolicy enhanced
-bcdedit /set disabledynamictick yes
+REM bcdedit /set useplatformtick yes
+REM bcdedit /set useplatformlock no
+REM bcdedit /set tscsyncpolicy enhanced
+REM bcdedit /set disabledynamictick yes
 bcdedit /set {current} description "OptimizedOS"
 fsutil behavior set disablelastaccess 1
 
